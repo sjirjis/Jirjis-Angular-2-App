c50eec048208b4955dfeb2532de2ccec25909d80
Steven Jirjis <sjirjis@tessituranetwork.com>
2017-07-02 22:42:13 -0700

    added server

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(express.static(path.join(__dirname)));
 
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
